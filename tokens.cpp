bc75e61770bdc61393793f1fcbf05c65a6aee0b7
Tirth Patel <tirthpatel61@outlook.com>
2024-02-21 17:43:59 +0530

    Define Token `get()` method

--- tokens.cpp
+++ tokens.cpp
@@ -1,4 +1,5 @@
 #include "tokens.h"
+#include <iostream>
 #include <string>
 
 std::string Token::to_string()
@@ -20,4 +21,35 @@ void putback(Token t)
         throw Buffer_full_error();
     buffer = t;
     full = true;
+}
+
+Token get()
+{
+    if (full)
+    {
+        full = false;
+        return buffer;
+    }
+
+    char c;
+    if (!(std::cin >> c))
+        return Token{'q'};  // Type 'q' indicates quit signal
+
+    switch (c)
+    {
+    case '+': case '-': case '*': case '/':
+    case '(': case ')': case ';': case 'q':
+        return Token{c};
+    case '.':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
+    {
+        std::cin.putback(c);
+        double value;
+        std::cin >> value;
+        return Token{'8', value};  // Type '8' indicates number token
+    }
+    default:
+        throw Bad_token_error {};
+    }
 }

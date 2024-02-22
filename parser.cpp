da5dc040face1e9ef77dcaea6a89441433c91afc
Tirth Patel <tirthpatel61@outlook.com>
2024-02-22 13:30:45 +0530

    Define `term()` parser function

--- parser.cpp
+++ parser.cpp
@@ -12,3 +12,22 @@ double primary()
     else
         throw Bad_expression_error();
 }
+
+double term()
+{
+    double left{primary()};
+    while (true)
+    {
+        Token t = get();
+        if (t.type == '+')
+            left += primary();
+        if (t.type == '-')
+            left -= primary();
+        else
+        {
+            putback(t);
+            break;
+        }
+    }
+    return left;
+}
